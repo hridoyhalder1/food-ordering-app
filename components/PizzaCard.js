1a90646c6a8ecf36afd943946b7e10cbfa9b0997
hridoyhalder1 <hridoyhalder91@gmail.com>
2023-03-20 22:10:38 +0600

    slider added

---
+++ components/PizzaCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/PizzaCard.module.css';
+
+
+const PizzaCard = () => {
+    return (
+        <div className={styles.container}>
+            Pizza
+            
+        </div>
+    );
+};
+
+export default PizzaCard;
