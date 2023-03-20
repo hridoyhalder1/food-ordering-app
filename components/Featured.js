2ddcbde2a31c3c5879664199e900cf0f91632238
hridoyhalder1 <hridoyhalder91@gmail.com>
2023-03-20 20:03:40 +0600

    food ordering app

---
+++ components/Featured.js
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import React, { useState } from 'react';
+import styles from '../styles/Featured.module.css';
+
+const Featured = () => {
+    const [index, setIndex] = useState(0);
+
+    const images = [
+        "/img/featured.png",
+        "/img/featured1.png",
+        "/img/featured2.png"
+    ];
+    const handleArrow = (direction) => {
+        if(direction === "l"){
+            setIndex(index !== 0 ? index-1 : 2)
+        }
+        if(direction === "r"){
+            setIndex(index !== 2 ? index+1 : 0)
+        }
+    }
+    console.log(index);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.arrowContainer} style={{ left: 0 }} onClick={()=> handleArrow("l")}>
+                <Image src='/img/arrowl.png' alt='' layout='fill' style={{objectFit: 'contain'}} ></Image>
+            </div>
+            <div className={styles.wrapper} style={{transform: `translateX(${-100*index}vw)`}}>
+                
+                    {images.map((img, i) => (
+                       <div className={styles.imgContainer} key={i} >
+                         <Image src={img} width={1000} height={500} alt='' style={{objectFit: 'contain'}} ></Image>
+                       </div>
+                    ))}
+                
+            </div>
+            <div className={styles.arrowContainer} style={{ right: 0 }} onClick={()=> handleArrow("r")}>
+                <Image src='/img/arrowr.png' alt='' layout='fill' style={{objectFit: 'contain'}} ></Image>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
---
+++ components/Navbar.js
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <div className={styles.callButton}>
+                    <Image src='/img/telephone.png' width={32} height={32} alt="telephone" draggable='false'></Image>
+                </div>
+                <div className={styles.texts}>
+                    <div className={styles.text}>ORDER NOW!</div>
+                    <div className={styles.text}><a href='tel:+8801863901163'>+8801863901163</a></div>
+                </div>
+            </div>
+            <div className={styles.item}>
+                <ul className={styles.list}>
+                    <li className={styles.listItem}>Home</li>
+                    <li className={styles.listItem}>Products</li>
+                    <li className={styles.listItem}>Menu</li>
+                    <Image src='/img/logo.png' alt='' width={160} height={69}></Image>
+                    <li className={styles.listItem}>Events</li>
+                    <li className={styles.listItem}>Blog</li>
+                    <li className={styles.listItem}>Contact</li>
+
+                </ul>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.cart}>
+                    <Image src='/img/cart.png' alt='' width={30} height={30}></Image>
+                    <div className={styles.counter}>
+                        2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
