1feccf9cbf8cb339ccbf7a48c743f5dea150cfd4
hridoyhalder1 <hridoyhalder91@gmail.com>
2023-03-22 00:44:41 +0600

    product page design setup

--- components/PizzaCard.js
+++ components/PizzaCard.js
@@ -6,7 +6,7 @@ import styles from '../styles/PizzaCard.module.css';
 const PizzaCard = () => {
     return (
         <div className={styles.container}>
-            <Image src='/img/pizza.png' alt='pizza' height="200" width="200"></Image>
+            <Image src='/img/pizza.png' alt='pizza' height="200" width="200" draggable="false"></Image>
             <h1 className={styles.title}>FIORI DI ZUCCA</h1>
             <span className={styles.price}>$19.99</span>
             <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
